ea611b3700f47739a5beb17aa5bde80c91cc9f54
superw23 <scymw7@nottingham.edu.cn>
2025-04-19 16:34:29 -0400

    chore: add visible trace for PR

--- project-root/prisma/migrations/20250417225003_a/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `cancelled` on the `Show` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Show" DROP COLUMN "cancelled";
