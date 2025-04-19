1722684785c8586893d4c932efb353c2e02d299f
ShiyaoSun827 <shiyao4@ualberta.ca>
2025-04-18 22:20:33 -0400

    兼容手机端 + 网页端扫描器fixed

--- project-root/prisma/migrations/20250415193316_a/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateEnum
-CREATE TYPE "ShowStatus" AS ENUM ('DRAFT', 'PUBLISHED', 'CANCELLED');
-
--- AlterTable
-ALTER TABLE "Show" ADD COLUMN     "status" "ShowStatus" NOT NULL DEFAULT 'DRAFT';
--- project-root/prisma/migrations/20250418031606_add_unique_to_qrcode/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[qrCode]` on the table `Ticket` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "Ticket_qrCode_key" ON "Ticket"("qrCode");
