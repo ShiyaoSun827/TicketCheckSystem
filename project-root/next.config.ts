c61581148ed00774435227a784e07f289ae44297
ShiyaoSun827 <shiyao4@ualberta.ca>
2025-04-19 22:38:37 -0400

    config +

--- project-root/next.config.ts
+++ project-root/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ['movies-images.tor1.cdn.digitaloceanspaces.com'], 
+  },
 };
 
 export default nextConfig;
